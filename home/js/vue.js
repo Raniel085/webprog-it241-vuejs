68b3b165ef5b39e1b72f9ff7aa634b82a9efd52a
Raniel <rqdelacruz@student.apc.edu.ph>
2026-01-10 23:02:57 +0800

    .

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,19 @@
-  const app = Vue.createApp({
+ const app = Vue.createApp({
    data() {
     return {
-     message: "Hello World!"
+      manyFoods: [
+        'img_burrito.svg',
+        'img_salad.svg',
+        'img_cake.svg',
+        'img_soup.svg',
+        'img_fish.svg',
+        'img_pizza.svg',
+        'img_rice.svg'
+      ]
     }
    }
   })
+  
   app.mount('#app')
- 
+
+
