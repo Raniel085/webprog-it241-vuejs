03ccaab07d8c52d445bfdb6a713985823f36599e
Raniel <rqdelacruz@student.apc.edu.ph>
2026-01-17 21:15:48 +0800

    Task 03 import my personal profile

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import PersonalProfile from './Components/PersonalProfile.vue'
 import FoodItem from './Components/FoodItem.vue'
 import FoodItem2 from './Components/FoodItem2.vue'
 
 const app = createApp(App)
 
-// Register BOTH components so they can be used in App.vue
+app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 
