32c7407345dd3c480b2c6649ade1456aebe6b395
Raniel <rqdelacruz@student.apc.edu.ph>
2026-01-27 19:47:02 +0800

    .

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,17 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// 1. Double-check if your folder is "components" or "Components"
-// If the error says it can't find "./components/...", try changing it to capital "C":
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import PersonalProfile from './components/PersonalProfile.vue' 
-import CommentForm from './components/CommentForm.vue'
-import Comment from './components/Comment.vue'
+import FoodItem from './Components/FoodItem.vue' // Capital C
+import FoodItem2 from './Components/FoodItem2.vue' // Capital C
+import PersonalProfile from './Components/PersonalProfile.vue' // Capital C
+import CommentForm from './components/CommentForm.vue' // Check if this is lower or upper
+import Comment from './components/Comment.vue' // Check if this is lower or upper
 
 const app = createApp(App)
 
-// 2. Register with consistent lowercase kebab-case names
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
