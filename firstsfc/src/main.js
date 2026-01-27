5668244dbea5d677dd16038fcb4f233f86003ee0
Raniel <rqdelacruz@student.apc.edu.ph>
2026-01-27 19:40:24 +0800

    .

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,16 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Change these to match your ACTUAL folder name (Capital 'C' or lowercase 'c')
 import FoodItem from './Components/FoodItem.vue'
 import FoodItem2 from './Components/FoodItem2.vue'
 import PersonalProfile from './Components/PersonalProfile.vue' 
-import CommentForm from './components/CommentForm.vue'
-import Comment from './components/Comment.vue'
+import CommentForm from './Components/CommentForm.vue' // Match the folder name!
+import Comment from './Components/Comment.vue'      // Match the folder name!
 
 const app = createApp(App)
 
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.component('personal-profile', PersonalProfile) // 2. Register it
+app.component('personal-profile', PersonalProfile)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 
